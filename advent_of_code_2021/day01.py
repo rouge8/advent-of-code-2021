8a217b24f61ac7ade7ca2e9548c080a93eece231
Andy Freeland <andy@andyfreeland.net>
2021-12-02 19:10:29 -0800

    Day 1 Part 2

--- advent_of_code_2021/day01.py
+++ advent_of_code_2021/day01.py
@@ -11,11 +11,20 @@ def part1(depths: list[int]) -> int:
     return count
 
 
+def part2(depths: list[int]) -> int:
+    sliding_windows = [
+        depths[i - 2] + depths[i - 1] + depths[i] for i in range(2, len(depths))
+    ]
+    return part1(sliding_windows)
+
+
 @click.command(name="day01")
 @click.argument("depths", type=click.File("r"))
 def main(depths):
     depths = [int(line) for line in depths]
+
     print(f"part1: {part1(depths)}")
+    print(f"part2: {part2(depths)}")
 
 
 if __name__ == "__main__":
--- tests/test_day01.py
+++ tests/test_day01.py
@@ -1,7 +1,17 @@
+import pytest
+
 from advent_of_code_2021.day01 import part1
+from advent_of_code_2021.day01 import part2
 
 
-def test_part1():
-    depths = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+@pytest.fixture
+def depths():
+    return [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
 
+
+def test_part1(depths):
     assert part1(depths) == 7
+
+
+def test_part2(depths):
+    assert part2(depths) == 5
